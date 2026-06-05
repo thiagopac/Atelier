fd16d209f6ba354458246f641213286449dad787
Thiago Pires <thiagopac@gmail.com>
2026-06-05 09:00:00 -0300

    Initial Commit

---
+++ Atelier/AtelierApp.swift
@@ -0,0 +1,18 @@
+//
+//  AtelierApp.swift
+//  Atelier
+//
+//  Created by Thiago Castro on 09/06/26.
+//
+
+
+import SwiftUI
+
+@main
+struct AtelierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atelier/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Atelier
+//
+//  Created by Thiago Castro on 09/06/26.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
